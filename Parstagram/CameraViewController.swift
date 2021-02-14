7a24e9c95e408b0b127b019886f05dc5685d6209
Peiwen Tan <peiwet1@uci.edu>
2021-02-14 02:51:26 -0800

    Paragram section 2

--- Parstagram/CameraViewController.swift
+++ Parstagram/CameraViewController.swift
@@ -57,7 +57,7 @@ class CameraViewController: UIViewController, UIImagePickerControllerDelegate, U
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         let image = info[.editedImage] as! UIImage
         let size = CGSize(width: 300, height: 300)
-        let scaleImage = image.af.imageScaled(to: size)
+        let scaleImage = image.af_imageAspectScaled(toFill: size)
         
         imageView.image = scaleImage
         
